a50894829b968d4f95f07b8daf6e38737da5ffed
Shankar Poncelet <shankx@mac.com>
2016-03-29 10:57:33 -0500

    Plugin initialized

---
+++ ConstantContactSubscribePlugin.php
@@ -0,0 +1,25 @@
+<?php
+namespace Craft;
+
+class ConstantContactSubscribePlugin extends BasePlugin
+{
+    function getName()
+    {
+         return Craft::t('Constant Contact Subscribe');
+    }
+
+    function getVersion()
+    {
+        return '1.0';
+    }
+
+    function getDeveloper()
+    {
+        return 'Shankar Poncelet';
+    }
+
+    function getDeveloperUrl()
+    {
+        return 'http://ShankxWebDev.com';
+    }
+}
